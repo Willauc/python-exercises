bae3b2d61a158a108cbb0b00649dfd430a3d97f8
William Turbide Auclair <william.turbide-auclair.1@ulaval.ca>
2025-06-06 15:31:45 -0400

    Init caractere frequent

---
+++ CompletePythonMasteryMoshExercises/caractere_plus_frequent.py
@@ -0,0 +1,6 @@
+# Auteur : William Turbide Auclair
+# Date : 06 juin 2025
+# Description : fonction qui retroune le caractére le plus fréquent dans un string.
+
+phrase = "this is a common interview question"
+
