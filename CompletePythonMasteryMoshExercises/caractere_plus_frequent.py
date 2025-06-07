74e6a21df1550100d5561988d244409a9ac01f70
William Turbide Auclair <williamauclair22@gmail.com>
2025-06-07 19:10:40 -0400

    Caractere_plus_frequent fini

--- CompletePythonMasteryMoshExercises/caractere_plus_frequent.py
+++ CompletePythonMasteryMoshExercises/caractere_plus_frequent.py
@@ -4,3 +4,24 @@
 
 phrase = "this is a common interview question"
 
+frequence = {}
+for car in phrase :
+    if car in frequence :
+        frequence[car] += 1
+    else :
+        frequence[car] = 1
+
+# En O(n)
+plusFrequent = ""
+for car in frequence :
+    if plusFrequent == "" :
+        plusFrequent = car
+    else :
+        if frequence[car] > frequence[plusFrequent]:
+            plusFrequent = car
+
+# Alternative en O(n log n) :
+#frequence_trier = sorted(frequence.items(), key=lambda x: x[1], reverse=True)
+#print(frequence_trier[0])
+
+print(plusFrequent, " est le caractere le plus frequent!")
