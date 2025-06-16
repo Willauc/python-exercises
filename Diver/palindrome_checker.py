6b048f25872c70f74f9b285a2dda7211eac2677c
William Turbide Auclair <williamauclair22@gmail.com>
2025-06-16 14:48:05 -0400

    panlindrome checker fini

--- Diver/palindrome_checker.py
+++ Diver/palindrome_checker.py
@@ -2,3 +2,33 @@
 # Date : 13 juin 2025
 # Description : Vérifier si une chaîne entrée par l’utilisateur est un palindrome.
 #               Ignorer les espaces et la ponctuation.
+from mercurial.utils.cborutil import BREAK
+
+# En O(n2)
+def palindrome_checker(mot):
+    mot = mot.lower().replace(" ", "")
+    while mot != "" :
+        if not mot[0].isalpha():
+            mot = mot[1:]
+        elif not mot[-1].isalpha():
+            mot = mot[:-1]
+        elif mot[0] != mot[-1] :
+            return False
+        mot = mot[1:-1]
+        print(mot)
+
+    return True
+
+
+# Version plus efficace trouver sur internet. en O(n)
+#def palindrome_checker(mot):
+#    lettres = [c.lower() for c in mot if c.isalpha()]
+#    return lettres == lettres[::-1]
+
+
+
+while True :
+    chaine = input("Entré un mot a vérifier (QUIT pour quitter le program) : ")
+    if chaine == "QUIT" :
+        break
+    print(palindrome_checker(chaine))
