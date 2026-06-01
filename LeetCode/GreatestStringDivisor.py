086d2e2cf64186dfafed327c75c6829d1d8db225
William Turbide Auclair <williamauclair22@gmail.com>
2026-06-01 15:21:31 -0400

    x2 leetcode exercise

---
+++ LeetCode/GreatestStringDivisor.py
@@ -0,0 +1,18 @@
+def gcdOfStrings(str1, str2):
+    """
+    :type str1: str
+    :type str2: str
+    :rtype: str
+    """
+    str3 = str2[:]
+    while str3 != "":
+        if str1 == str3 * (len(str1) // len(str3)) and len(str1) % len(str3) == 0 and str2 == str3 * (len(str2) // len(str3)) and len(str2) % len(str3) == 0:
+            return str3
+        else:
+            str3 = str3[:-1]
+    return ""
+
+if __name__ == '__main__':
+    result = gcdOfStrings("ABABAB", "ABAB") == "AB"
+    print(gcdOfStrings("ABABAB", "ABAB"))
+    print("test 1: " + str(result))
---
+++ LeetCode/MergeStringAlt.py
@@ -0,0 +1,47 @@
+def mergeAlternately(word1, word2):
+    """
+    :type word1: str
+    :type word2: str
+    :rtype: str
+    """
+    word1, word2 = word1.lower().replace(" ", ""), word2.lower().replace(" ", "")
+
+    final_word = ""
+
+    while word1 != "" and word2 != "":
+        final_word += word1[0]
+        final_word += word2[0]
+
+        word1 = word1[1:]
+        word2 = word2[1:]
+
+        if word1 == "" and word2 != "":
+            final_word += word2
+            word2 = ""
+        elif word1 != "" and word2 == "":
+            final_word += word1
+            word1 = ""
+    return final_word
+
+
+# Autre solution trouver sur leetcode.
+def mergeAlternatelySolution(word1, word2):
+    """
+    :type word1: str
+    :type word2: str
+    :rtype: str
+    """
+    result = []
+    i = 0
+    while i < len(word1) or i < len(word2):
+        if i < len(word1):
+            result.append(word1[i])
+        if i < len(word2):
+            result.append(word2[i])
+        i += 1
+    return ''.join(result)
+
+
+if __name__ == "__main__":
+    result = mergeAlternately("abc", "def") == "adbecf"
+    print("Test 1: " + str(result))
